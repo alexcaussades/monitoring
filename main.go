b757bacfdbf2c2e6130ce2024038af487841d011
alexcaussades <alexcaussades@gmail.com>
2021-10-30 12:07:05 +0200

    Revert "monitoring frist"
    This reverts commit b2a16d214f8f274e5f6f6b51405a9df96f5f9459.

--- main.go
+++ main.go
@@ -18,30 +18,55 @@ func urlinport(url string) (string, error) {
 	return resp.Status, nil
 }
 
-func Caseurl() {
-	resp, _ := urlinport("https://www.alexcaussades.com")
+func getmap() map[int]string {
+
+	m := make(map[int]string)
+	m[0] = "https://www.alexcaussades.com"
+	m[1] = "https://ouioweb.com/"
+	m[2] = "https://myalternos.fr/"
+	return m
+
+}
+
+func Caseurl(value string) {
+
+	/**
+	* changement de formule sur la function
+	 */
+
+	resp, error := urlinport(value)
 	discord.WebhookURL = webhookperso.TokenPerso()
-	
+	now := time.Now()
 	if resp != "No access the adresse web" {
 		switch resp {
 		case "200 OK":
-			fmt.Println("ok ", discord.Say("Hello, world!"))
+			
 		case "404":
-			fmt.Println("404")
+			fmt.Println(discord.Say(now.Format("15:04:05") + " " + resp + " => erreur: 404"))
+		case "500":
+			fmt.Println(discord.Say((now.Format("15:04:05") + " " + resp + " => erreur: 500")))
+		case "503":
+			fmt.Println(discord.Say((now.Format("15:04:05") + " " + resp + " => erreur: 500")))
+		case "504":
+			fmt.Println(discord.Say((now.Format("15:04:05") + " " + resp + " => erreur: 504")))
 		default:
 			fmt.Println("default", resp)
 		}
 	} else {
-		fmt.Println("No access the adresse web")
+		fmt.Println(discord.Say((now.Format("15:04:05") + " " + error.Error() )))
 	}
 
 }
 
 func main() {
-
-	c := time.Tick(1 * time.Second)
+	m := getmap()
+	c := time.Tick(60 * time.Second)
 	for now := range c {
-		fmt.Println(now.Format("15:04:05"))
-		Caseurl()
+		for _, i := range m {
+			fmt.Println(now.Format("15:04:05"))
+			Caseurl(i)
+		}
+
 	}
+
 }
